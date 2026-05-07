cc1fc9c489cdfa41764a5873fe3575afe888cad2
lizhijie9413-art <lizhijie9413@gmail.com>
2026-05-08 02:10:39 +0800

    Update layout.tsx

--- knowledge-investment-site/app/layout.tsx
+++ knowledge-investment-site/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Knowledge Capital",
-  description: "知识投资博主网站",
+  description: Knowledge Investment Blogger Website,
 };
 
 export default function RootLayout({
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="zh-CN">
+   <html lang="en">
       <body>{children}</body>
     </html>
   );
